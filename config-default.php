81e88d4da56d506b69117908e6b52df2fd89bad3
Rick Clark <quixrick@gmail.com>
2022-04-29 15:40:56 -0600

    Add gitignore and config-default files

---
+++ config-default.php
@@ -0,0 +1,11 @@
+<?php
+
+
+##  MAKE A COPY OF THIS FILE, NAME IT `config.php` AND CONFIGURE IT FOR YOUR SERVER
+
+$settings = array();
+
+$settings['db_host'] = 'localhost';
+$settings['db_user'] = 'username';
+$settings['db_pass'] = 'password';
+$settings['db_name'] = 'db_name';
