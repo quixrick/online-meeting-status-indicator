655d286c1715576b57650d5662f477c18ca9e4d8
Rick Clark <quixrick@gmail.com>
2022-05-02 07:38:51 -0600

    Create privacy policy page.

---
+++ privacy.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+# PRIVACY POLICY
+# /privacy.php
+
+
+print '
+<b>Email addresses are used for login, billing, security and account identity purposes only.</b>
+
+<br><br>You will get emails from us for:
+<ul>
+  <li>Setup/Confirmation of an account</li>
+  <li>Resetting Password</li>
+  <li>Payment information
+</ul>
+<br><br>
+
+Unless you specifically opt-in to receive updates from us, we won\'t send emails about changes, new features or enhancements either.  If you do want to receive these types of notifications, you can opt-in from your account page.
+
+<br><br>
+
+We do not give out your email address to anyone.';
