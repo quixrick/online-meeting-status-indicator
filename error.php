c15436cc3c902bc5f93a0dc854681b997ee6e756
Rick Clark <quixrick@gmail.com>
2022-05-03 21:20:35 -0600

    Add error page

---
+++ error.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+# ERROR PAGE
+# /error.php
+
+
+$error = '';
+if ((isset($_GET['error'])) && (!empty($_GET['error']))) {
+	$error = '<br><ul>'.json_decode($_GET['error']).'</ul><br>';
+}
+
+print $error;
