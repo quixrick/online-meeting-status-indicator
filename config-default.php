7fc2b5a34eef97dfb74f721feb283ef53b7eb397
Rick Clark <quixrick@gmail.com>
2022-05-06 07:58:00 -0600

    Add PHP JWT file and add a slot in the config file for the JWT token.

--- config-default.php
+++ config-default.php
@@ -5,7 +5,21 @@
 
 $settings = array();
 
+
+
+
+
+
+// DATABASE SETTINGS
 $settings['db_host'] = 'localhost';
 $settings['db_user'] = 'username';
 $settings['db_pass'] = 'password';
 $settings['db_name'] = 'db_name';
+
+
+
+
+
+
+// JWT SETTINGS
+$settings['jwt_token'] = 'xxxxxxxxxxxxxxxx';
