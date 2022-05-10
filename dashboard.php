b2ecc9c4b23a6326a96d21e8008aca1803fc1819
Rick Clark <quixrick@gmail.com>
2022-05-09 19:05:52 -0600

    Pull the statuses from the database and display them on the dashboard.

--- dashboard.php
+++ dashboard.php
@@ -3,4 +3,79 @@
 # DASHBOARD
 # /dashboard.php
 
-print 'Welcome to the dashboard.';
+require_once 'header.php';
+require_once 'header_close.php';
+require_once 'footer.php';
+
+print preg_replace('~(\.body\s*{\s*)display: flex~', '$1display: block', $header);
+
+print '
+<script>
+
+var jwt = localStorage.getItem(\'jwt\');
+
+$.ajax({
+	type: "POST",
+	url: "dashboard_api.php",
+	data: { jwt: jwt }
+}).done(function( data ) {
+
+
+
+	console.log( data );
+	console.log(data.items);
+
+
+
+	$.each(data.items, function(key, status_array) {
+		console.log(status_array);
+		
+		var status_format = jQuery.parseJSON(status_array.status_format);
+		console.log(status_format);
+		
+		
+		console.log(status_array.status_message);
+		
+		if (status_array.active_status == 1) {
+			$(".statuses_active").append("<div style=\"width: 200px; border-radius: 10px; margin: 10px; text-align: center; padding: 10px; font-family: " + status_format[\'font-family\'] + "; font-weight: " + status_format[\'font-weight\'] + "; color: " + status_format.color + "; background-color: " + status_format[\'background-color\'] + "\">" + status_format.icon + " &nbsp; " + status_array.status_message + "</div>");
+		}
+		else if (status_array.default_status == 1) {
+			$(".statuses_default").append("<div style=\"width: 200px; border-radius: 10px; margin: 10px; text-align: center; padding: 10px; font-family: " + status_format[\'font-family\'] + "; font-weight: " + status_format[\'font-weight\'] + "; color: " + status_format.color + "; background-color: " + status_format[\'background-color\'] + "\">" + status_format.icon + " &nbsp; " + status_array.status_message + "</div>");
+		}
+		else {
+			$(".statuses_other").append("<div style=\"width: 200px; border-radius: 10px; margin: 10px; text-align: center; padding: 10px; font-family: " + status_format[\'font-family\'] + "; font-weight: " + status_format[\'font-weight\'] + "; color: " + status_format.color + "; background-color: " + status_format[\'background-color\'] + "\">" + status_format.icon + " &nbsp; " + status_array.status_message + "</div>");
+		}
+		
+		
+	});
+
+
+
+});
+</script>';
+
+
+
+print $header_close;
+
+
+
+
+print '
+
+<div id="statuses_active" class="statuses_active">Active Status</div>
+
+<br><br>
+
+<div style="display: block;" id="statuses_default" class="statuses_default">Default Status</div>
+
+<br><br>
+
+<div id="statuses_other" class="statuses_other">Other Statuses</div>
+
+';
+
+
+
+
+print $footer;
---
+++ dashboard_api.php
@@ -0,0 +1,257 @@
+﻿<?php
+
+# DASHBOARD
+# /dashboard.php
+
+
+
+// ************************************ { 2022-05-06 - RC } ************************************
+//INCLUDE THE JWT FILE 
+// *********************************************************************************************
+require_once 'jwt.php';
+
+
+
+
+
+
+// ************************************ { 2022-04-29 - RC } ************************************
+// LOAD THE CONFIG FILE AND CONNECT TO THE DATABASE
+// *********************************************************************************************
+require_once 'config.php';
+require_once 'header.php';
+require_once 'footer.php';
+
+$conn = new PDO("mysql:host=".$settings['db_host'].";dbname=".$settings['db_name'].";",$settings['db_user'],$settings['db_pass']);
+
+
+
+
+
+
+// ************************************ { 2022-05-03 - RC } ************************************
+// SET THE DEFAULTS
+// *********************************************************************************************
+$errors = array();
+
+if ((isset($_POST['jwt'])) && (!empty($_POST['jwt']))) {
+	$jwt = $_POST['jwt'];
+}
+
+
+
+
+
+
+$user_array = get_jwt_user_info($jwt, $settings['jwt_token'], $errors);
+
+
+
+
+
+
+if (verify_user_info($user_array, $errors, $conn)) {
+	// GET THE LIST OF STATUSES
+	$status_list = get_status_list($user_array, $errors, $conn);
+	print header('Content-Type: application/json');
+	print json_encode(array('items' => $status_list));
+}
+
+
+
+
+
+
+function get_status_list($user_array, $errors, $conn) {
+
+
+
+	// ************************************ { 2022-05-09 - RC } ************************************
+	// SET THE DEFAULTS
+	// *********************************************************************************************
+	$return_array = array();
+
+
+
+
+
+
+	// ************************************ { 2022-05-09 - RC } ************************************
+	// PULL THE STATUSES FOR THE USER FROM THE DATABASE
+	// *********************************************************************************************
+	$q_get_status_list = $conn->prepare("
+		SELECT
+			Reference,
+			Status_Message,
+			Status_Start_Utc,
+			Status_End_Utc,
+			Default_Status,
+			Active_Status,
+			Status_Format
+		FROM
+			statuses
+		WHERE
+			Parent = :parent
+		ORDER BY
+			Active_Status DESC,
+			Default_Status DESC,
+			Status_Message ASC
+	");
+
+	$q_get_status_list->execute(
+		array(
+			':parent' => $user_array['user_id']
+		)
+	);
+
+	$row_get_status_list = $q_get_status_list->fetchAll(PDO::FETCH_ASSOC);
+
+	foreach ($row_get_status_list AS $get_status_list) {
+
+
+
+		$status_array = array();
+		$status_array['reference'] = $get_status_list['Reference'];
+		$status_array['status_message'] = $get_status_list['Status_Message'];
+		$status_array['status_start'] = $get_status_list['Status_Start_Utc'];
+		$status_array['status_end'] = $get_status_list['Status_End_Utc'];
+		$status_array['default_status'] = $get_status_list['Default_Status'];
+		$status_array['active_status'] = $get_status_list['Active_Status'];
+		$status_array['status_format'] = $get_status_list['Status_Format'];
+
+
+
+		$return_array[] = $status_array;
+
+
+
+	}
+
+
+
+	return $return_array;
+
+
+
+}
+
+
+
+
+
+
+
+
+function verify_user_info($user_array, &$errors, $conn) {
+
+
+
+	// ************************************ { 2022-05-06 - RC } ************************************
+	// SET THE DEFAULTS
+	// *********************************************************************************************
+	$user_count = 0;
+
+
+
+
+
+
+	// ************************************ { 2022-05-06 - RC } ************************************
+	// CHECK TO MAKE SURE THE STUFF IN THE TOKEN MATCHES WHAT IS IN THE DATABASE
+	// *********************************************************************************************
+	$q_verify_user_info = $conn->prepare("
+		SELECT
+			COUNT(*) AS user_count
+		FROM
+			users
+		WHERE
+			Reference = :user_id AND
+			Email = :username
+	");
+
+	$q_verify_user_info->execute(
+		array(
+			':user_id' => $user_array['user_id'],
+			':username' => $user_array['username']
+		)
+	);
+
+	$row_verify_user_info = $q_verify_user_info->fetchAll(PDO::FETCH_ASSOC);
+
+	foreach ($row_verify_user_info AS $verify_user_info) {
+
+		$user_count = $verify_user_info['user_count'];
+
+	}
+
+
+
+	if ($user_count == 0) {
+
+		$errors[] = 'Authentication Error.<br><br>Please try signing out and back in again. [#da1]';
+		return false;
+
+	}
+
+
+
+	return true;
+
+
+
+}
+
+
+
+function get_jwt_user_info($jwt, $server_key, &$errors) {
+
+
+
+	// ************************************ { 2016-04-- RC } ************************************
+	// VERIFY THE USER FROM THE JAVASCRIPT WEB TOKEN
+	// *********************************************************************************************
+	try {
+
+		$jwt_object = JWT::decode($jwt, $server_key);
+
+	}
+	catch (Exception $e) {
+
+		$errors[] = 'Authentication Error.<br><br>Please try signing out and back in again. [#da2]';
+		handle_errors($errors);
+		return;
+
+	}
+
+
+
+	return (array)$jwt_object;
+
+
+
+}
+
+
+
+
+
+
+// ************************************ { 2022-05-06 - RC } ************************************
+// LOG THE USER IN OR REDIRECT TO THE REGISTRATION PAGE AND SUPPLY AN ERROR MESSAGE
+// *********************************************************************************************
+function handle_errors($errors = array()) {
+
+
+
+	if (!empty($errors)) {
+
+		$error_string = json_encode('<li>'.implode('<li>', $errors));
+
+		header("Location: error.php?error=".$error_string);
+		return;
+
+	}
+
+
+
+}
